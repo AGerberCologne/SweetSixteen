c1d52fcad8ff176e03a6a054e764fa09fe9fced8
AGerberCologne <agerber2@smail.uni-koeln.de>
2016-06-27 16:58:17 +0200

    Revert "Das ist mein zweiter Test"
    This reverts commit 9eb3b802ce28f59ebab5a2096d1e17fde93283cc.

--- Sweet Sixteen/src/pp2016/team16/shared/ChangeLevelMessage.java
+++ Sweet Sixteen/src/pp2016/team16/shared/ChangeLevelMessage.java
@@ -2,11 +2,10 @@ package pp2016.team16.shared;
 
 public class ChangeLevelMessage extends MessageObject {
 	int[][] map;
-	boolean login;
+	boolean login = false;
 	
 	public ChangeLevelMessage(){
 		this.map = new int[16][16];
-		this.login = false;
 	}
 	
 	public ChangeLevelMessage(int i){

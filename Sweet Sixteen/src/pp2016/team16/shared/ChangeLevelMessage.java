9eb3b802ce28f59ebab5a2096d1e17fde93283cc
AGerberCologne <agerber2@smail.uni-koeln.de>
2016-06-27 16:30:36 +0200

    Das ist mein zweiter Test

--- Sweet Sixteen/src/pp2016/team16/shared/ChangeLevelMessage.java
+++ Sweet Sixteen/src/pp2016/team16/shared/ChangeLevelMessage.java
@@ -2,10 +2,11 @@ package pp2016.team16.shared;
 
 public class ChangeLevelMessage extends MessageObject {
 	int[][] map;
-	boolean login = false;
+	boolean login;
 	
 	public ChangeLevelMessage(){
 		this.map = new int[16][16];
+		this.login = false;
 	}
 	
 	public ChangeLevelMessage(int i){

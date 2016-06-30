e05d9e26991b1c6c9398f7729a5366b86ece6f6a
Markus <Markus@172.29.233.209>
2016-06-30 16:09:57 +0200

    Methoden angepasst

--- Sweet Sixteen/src/pp2016/team16/server/map/Felder.java
+++ Sweet Sixteen/src/pp2016/team16/server/map/Felder.java
@@ -4,7 +4,8 @@ public class Felder {
 	
 	public static final int WAND = 0;
 	public static final int BODEN = 1;
-	public static final int MONSTER = 2;
+	public static final int MONSTER1 = 2;
+	public static final int MONSTER2 = 8;
 	public static final int MONSTERMITSCHLUESSEL = 3;
 	public static final int START = 4;
 	public static final int TRANK = 5;

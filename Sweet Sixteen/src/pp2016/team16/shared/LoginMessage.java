fa659cef959990c6c66a397e8798e4b75f370257
Ann-Catherine <Ann-Catherine@AnCa-Laptop.fritz.box>
2016-07-02 22:32:00 +0200

    Alina, schau mal nochmal in die Klasse

--- Sweet Sixteen/src/pp2016/team16/shared/LoginMessage.java
+++ Sweet Sixteen/src/pp2016/team16/shared/LoginMessage.java
@@ -3,9 +3,11 @@ package pp2016.team16.shared;
 public class LoginMessage extends MessageObject{
 	String name;
 	String passwort;
-	public LoginMessage(String name, String passwort){
+	int artVonAnmeldung;
+	public LoginMessage(String name, String passwort, int artVonAnmeldung){
 		this.name = name;
-		this.passwort = name;
+		this.passwort = name;//alina, muss das nicht Passwort sein ??
+		this.artVonAnmeldung=artVonAnmeldung;
 		
 	}
 

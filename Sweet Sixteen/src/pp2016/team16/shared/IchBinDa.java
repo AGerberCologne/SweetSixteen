dddd0ddef7c891e0177810855cdbe192ad876338
Ann-Catherine <Ann-Catherine@AnCa-Laptop>
2016-07-13 20:08:18 +0200

    Ichbinda auskommentiert

--- Sweet Sixteen/src/pp2016/team16/shared/IchBinDa.java
+++ Sweet Sixteen/src/pp2016/team16/shared/IchBinDa.java
@@ -1,12 +1,20 @@
 package pp2016.team16.shared;
-
+ /**
+  * 
+  * @author Ann-Catherine Hartmann 6038514
+  *
+  */
 public class IchBinDa extends MessageObject{
 
 	/**
-	 * 
+	 *generierete serialVerionID
 	 */
 	private static final long serialVersionUID = -4801926308794934580L;
-
+	/**
+	 *	Dies ist der Messagetyp für die periodische Ichbinda-Nachricht und hat keinen Inhalt
+	 * 
+	 * @author Ann-Catherine Hartmann 6038514
+	 */
 	public IchBinDa(){
 		
 	}

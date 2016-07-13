2f864b3e9de1f580d13f4c9b6bc7dd3865313e36
Simon <Simon@172.29.220.135>
2016-07-13 18:33:24 +0200

    Kommentare

--- Sweet Sixteen/src/pp2016/team16/client/gui/Highscore.java
+++ Sweet Sixteen/src/pp2016/team16/client/gui/Highscore.java
@@ -5,176 +5,72 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedList;
-
 import javax.imageio.ImageIO;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import pp2016.team16.client.gui.LoginDialog;
 
 
 /**
- * Klasse für das Panel um den Highscore anzuzeigen
- * @author Simon Nietz, Matr_Nr: 5823560
+ * @author Diese Klasse wurde aus dem alten Spiel uebernommen.
  *
  */
 
 public class Highscore extends JPanel {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 9029630360878826482L;
-	//private static final long serialVersionUID = 1L;
-	
-	//private LinkedList<HighScoreElement> highScore;
+
 	public LinkedList <String> highScore;
-	
-	
-	
+
+
+	/**
+	 * @author Dieser Konstruktor wurde aus dem alten Spiel uebernommen.
+	 */
 	public Highscore(){
 		highScore = new LinkedList<String>();
-
-		/*highScore = new LinkedList<String>();
-		
-		try {
-			FileReader reader = new FileReader(new File("highscore.txt"));
-			int c;
-			String line = "";
-			
-			
-			while((c = reader.read()) != -1){				
-				
-				if(c == '\n'){
-					String[] temp = line.split("\t");
-					highScore.add(new HighScoreElement(Integer.parseInt(temp[0].trim()), temp[1].trim()));					
-					line = "";
-				}else{
-					line += (char) c;
-				}
-			}
-			
-			reader.close();
-			
-		} catch (IOException e) {
-			System.out.println("Highscore konnte nicht gelesen werden");
-		}
-		while(highScore.size() < 10){
-			highScore.add(new HighScoreElement(1000, "Anonym"));	
-		}*/
 	}
-		
-	/**
-	 * Methode um einen Spieler in die Highscore Liste mit aufzunehmen
-	 * @author Simon Nietz, Matr_Nr: 5823560
-	 * @param zeit ist der Highscore
-	 */
-	
-	/*public void addSpielerToHighScore(int zeit){
-		String name;
-		if(LoginDialog.succeeded==true ){
-			 name = LoginDialog.getUsername();
-		}else{
-			 name = JOptionPane.showInputDialog("Bitte geben Sie Ihren Namen ein:");
-		}
-		for(int i = 0; i < highScore.size(); i++){
-			if(highScore.get(i).zeit > zeit){
-				highScore.add(i, new HighScoreElement(zeit, name));
-				i = highScore.size();
-			}
-		}
-		// der name und die zeit wird in die Liste Highscore.txt eingetragen
-		try {
-			FileWriter writer = new FileWriter(new File("highscore.txt"));
-			for(int i = 0; i < 10; i++){
-				writer.write(highScore.get(i).zeit + "\t" + highScore.get(i).name + "\n");
-			}			
 
-			writer.close();
-			
-		} catch (IOException e) {
-			System.out.println("Highscore konnte nicht geschrieben werden");
-		}
-		
-	}*/
+
+
 	/**
-	 * getter für den Highscore
-	 * @author Simon Nietz, Matr_Nr: 5823560
-	 * @return Die Highscore Liste
-	 */
-	/*
-	public LinkedList<HighScoreElement> getHighScore(){
-		return highScore;
-	}
-	*/
-	/**
-	 * paint methode um den Highscore anzuzeigen
-	 * @author Simon Nietz, Matr_Nr: 5823560
+	 * @author Diese Methode wurde aus dem alten Spiel uebernommen.
 	 */
 	public void paint(Graphics g){
-		
+
 		Image img = null, boden = null;
-		
+
 		try{
-			System.out.println("Versuch");
 			img = ImageIO.read(new File("img//highscore.png"));
 			boden = ImageIO.read(new File("img//status.png"));
-		
-		
-		
-		for(int i = 0; i < 17; i++){
-			for(int j = 0; j < 18; j++){
-				g.drawImage(boden, 32*i,32*j,null);
-				System.out.println("Fenster wird gezeichnet");
+
+
+
+			for(int i = 0; i < 17; i++){
+				for(int j = 0; j < 18; j++){
+					g.drawImage(boden, 32*i,32*j,null);
+					System.out.println("Highscore wird gezeichnet");
+				}
 			}
-		}
-		
-		g.drawImage(img, 0, 0, null);
-		System.out.println("Was auch immer das macht");
-		g.setColor(Color.WHITE);
-		
-		for(int k = 1;k<6;k++){
-		if(!highScore.isEmpty()){
-			String m = highScore.removeFirst();
-			System.out.println("Highscorename:"+m);
-			
-			g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
-			g.drawString((k)+".: "+ m, 80, 150 + 30 * (k + 1));
-			
-			//g.drawString("" + zeit, 400, 150 + 30 * (i + 1));
-			}}
-		
-		
-	
+
+			g.drawImage(img, 0, 0, null);
+			g.setColor(Color.WHITE);
+
+			for(int k = 1;k<6;k++){
+				if(!highScore.isEmpty()){
+					String m = highScore.removeFirst();
+					System.out.println("Highscorename:"+m);
+
+					g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
+					g.drawString((k)+".: "+ m, 80, 150 + 30 * (k + 1));
+
+				}}
+
+
+
 		}catch(IOException e){ }
-		
+
 	}
-	
+
 }
 
-/**
- * Klasse für HighScoreElement
- * @author Simon Nietz, Matr_Nr: 5823560
- *
- */
 
-class HighScoreElement {
-	
-	String name;
-	int zeit;
-	
-	/** Konstruktor für HighScoreElement
-	 * @author Simon Nietz, Matr_Nr: 5823560
-	 * @param punkte Die Punkte des Spielers
-	 * @param name Der Name des Spielers 
-	 */
-	
-	public HighScoreElement(int punkte, String name){
-		this.name = name;
-		this.zeit = punkte;
-	}
-	
-}
--- Sweet Sixteen/src/pp2016/team16/client/gui/LoginDialog.java
+++ Sweet Sixteen/src/pp2016/team16/client/gui/LoginDialog.java
@@ -7,274 +7,198 @@ import javax.swing.border.*;
 
 import pp2016.team16.client.engine.ClientEngine;
 import pp2016.team16.client.gui.HindiBones;
- 
+
 /**
- * Klasse für den LoginDialog
+ * <Klasse für den LoginDialog, es speichert bei einer Neuanmeldung, Name und Passwort in einer Liste.
+ * Bei einem Einloggen, wird abgefragt ob die eingegebenen Daten zulaessig sind.>
+ * 
  * @author Simon Nietz, Matr_Nr: 5823560
  *
  */
 
 public class LoginDialog extends JDialog {
- 
-	//public static ArrayList<String> User = new ArrayList<String>();
-	//public static ArrayList<String> Passwort = new ArrayList<String>();
 	public ClientEngine engine;
-	
+
 	private HindiBones fenster;
 	private static final long serialVersionUID = 1L;
 	private static JTextField tfUsername;
-    private JPasswordField pfPassword;
-    public JLabel lbUsername;
-    private JLabel lbPassword;
-    private JButton btnLogin;
-    private JButton btnAnmelden;
-    private JButton btnCancel1;
-    public static boolean succeeded;
-    public static boolean test;
-    public static boolean test2;
-    public String passwortname;
-    		
-    /**
-     * @author Simon Nietz, Matr_Nr: 5823560
-     * @param parent erweitere das LoginFenster aus HindiBones
-     */
-    public LoginDialog(Frame parent, HindiBones fenster) {
-        super(parent, "Login", true);
-        this.fenster = fenster;
-    
-        // erzeuge ein neues JPanel
-        // ordne die Buttons mithilfe GridBagLayout
-        JPanel panel = new JPanel(new GridBagLayout());
-        GridBagConstraints cs = new GridBagConstraints();
-        
-        
-        
-        cs.fill = GridBagConstraints.HORIZONTAL;
-        
-        //erstelle die Buttons Benutername, Passwort mit den zugehörigen Textfeldern 
-        lbUsername = new JLabel("Benutzername: ");
-        cs.gridx = 0;
-        cs.gridy = 0;
-        cs.gridwidth = 1;
-        panel.add(lbUsername, cs);
- 
-        tfUsername = new JTextField(20);
-        cs.gridx = 1;
-        cs.gridy = 0;
-        cs.gridwidth = 2;
-        panel.add(tfUsername, cs);
- 
-        lbPassword = new JLabel("Passwort: ");
-        cs.gridx = 0;
-        cs.gridy = 1;
-        cs.gridwidth = 1;
-        panel.add(lbPassword, cs);
- 
-        pfPassword = new JPasswordField(20);
-        cs.gridx = 1;
-        cs.gridy = 1;
-        cs.gridwidth = 2;
-        panel.add(pfPassword, cs);
-        panel.setBorder(new LineBorder(Color.GRAY));
- 
-        btnLogin = new JButton("Login");
- 
-        btnLogin.addActionListener(new ActionListener() {
- 
-        	public void actionPerformed(ActionEvent e){
-        		if(getUsername().equals("") || getPassword().equals("")){
-        			JOptionPane.showMessageDialog(LoginDialog.this,
-                            "Gib einen Benutzernamen und ein Passwort ein!",
-                            "Anmelden",
-                            JOptionPane.ERROR_MESSAGE);
-        		}else{
-        		try {
-					test2 = fenster.engine.login(2, getUsername(), getPassword());
-				} catch (InterruptedException e1) {
-				}
-        	if (test2 == true){
-        		JOptionPane.showMessageDialog(LoginDialog.this,
-                        "Hi " + getUsername() + "! Du hast dich erfolgreich eingeloggt.",
-                        "Login",
-                        JOptionPane.INFORMATION_MESSAGE);
-                test = true;
-                
-                dispose();    		
-        	} else  {                
-                JOptionPane.showMessageDialog(LoginDialog.this,
-                        "Ungültiger Benutzername oder Passwort",
-                        "Login",
-                        JOptionPane.ERROR_MESSAGE);
-                // Benutzername und Passwort zuruecksetzen
-                tfUsername.setText("");
-                pfPassword.setText("");
-                test = false;  
-        	}
-        		}
-        	}
-        	// abfragen, ob der eingegeben Benutzername existiert und das Passwort stimmt
-       /*     public void actionPerformed(ActionEvent e) {
-                if (Loginn.authenticate(getUsername(), getPassword())) {
-                    JOptionPane.showMessageDialog(LoginDialog.this,
-                            "Hi " + getUsername() + "! Du hast dich erfolgreich eingeloggt.",
-                            "Login",
-                            JOptionPane.INFORMATION_MESSAGE);
-                    succeeded = true;
-                    test= true;
-                    dispose();
-                } else {
-                    JOptionPane.showMessageDialog(LoginDialog.this,
-                            "Ungültiger Benutzername oder Passwort",
-                            "Login",
-                            JOptionPane.ERROR_MESSAGE);
-                    // Benutzername und Passwort zuruecksetzen
-                    tfUsername.setText("");
-                    pfPassword.setText("");
-                    succeeded = false;
- 
-                }
-            } */
-        });
-        //moeglichkeit ohne sich einzuloggen zu spielen
-  /*      btnCancel1 = new JButton("Spiele als Gast");
-        btnCancel1.addActionListener(new ActionListener() {
- 
-            public void actionPerformed(ActionEvent e) {
-                test = true;
-                dispose();
-               
-            }
-         
-        });*/
-        
-        //neuen Benutzer erstellen
-        btnAnmelden = new JButton("Neu Anmelden");
-        btnAnmelden.addActionListener(new ActionListener(){
-        	public void actionPerformed(ActionEvent e){
-        		if(getUsername().equals("") || getPassword().equals("")){
-        			JOptionPane.showMessageDialog(LoginDialog.this,
-                            "Gib einen Benutzernamen und ein Passwort ein!",
-                            "Anmelden",
-                            JOptionPane.ERROR_MESSAGE);
-        		}else {
-        		
-        		try {
-					test2 = fenster.engine.login(1, getUsername(), getPassword());
-				} catch (InterruptedException e1) {
-				}
-        		
-        		if(test2 == true){
-        			JOptionPane.showMessageDialog(LoginDialog.this,
-                            "Hi " + getUsername() + "! Du hast dich erfolgreich neu angemeldet",
-                            "Anmeldung",
-                            JOptionPane.INFORMATION_MESSAGE);
-                    test = false;
-                    tfUsername.setText("");
-                    pfPassword.setText("");
-        		} else {      			
-                    
-                    JOptionPane.showMessageDialog(LoginDialog.this,
-                            "Der Benutzername ist bereits vergeben!",
-                            "Login",
-                            JOptionPane.ERROR_MESSAGE);
-        			test = false;
-        		}
-        	}
-        	}
-      
-       /* 	public void actionPerformed(ActionEvent e) {   
-        		if(User.contains(getUsername())){
-        			JOptionPane.showMessageDialog(LoginDialog.this,
-                            "Der Benutzername ist bereits vergeben!",
-                            "Login",
-                            JOptionPane.ERROR_MESSAGE);
-        			succeeded = false;
-        		}else if(getUsername().equals("") || getPassword().equals("")){
-        			JOptionPane.showMessageDialog(LoginDialog.this,
-                            "Bitte gib ein Namen und ein Passwort ein",
-                            "Login",
-                            JOptionPane.ERROR_MESSAGE);
-        			succeeded = false;
-        			
-        		}else {
-        		User.add(getUsername());
-        		Passwort.add(getPassword());
-        		JOptionPane.showMessageDialog(LoginDialog.this,
-                        "Hi " + getUsername() + "! Du hast dich erfolgreich neu angemeldet",
-                        "Anmeldung",
-                        JOptionPane.INFORMATION_MESSAGE);
-                succeeded = false;
-                tfUsername.setText("");
-                pfPassword.setText("");
-                test= true;
-        		}
-        		
-        	} */
-        });
-       
-        JPanel bp = new JPanel();
-        bp.add(btnLogin);
-    //    bp.add(btnCancel1);
-        bp.add(btnAnmelden);
- 
-        getContentPane().add(panel, BorderLayout.CENTER);
-        getContentPane().add(bp, BorderLayout.PAGE_END);
- 
-        pack();
-        setResizable(false);
-        setLocationRelativeTo(parent);
-    }
- 
-    public static String getUsername() {
-        return tfUsername.getText().trim();
-    }
- 
-    public String getPassword() {
-        return new String(pfPassword.getPassword());
-       
-    }
- 
-    public static boolean isSucceded(){
-    	return test;
-    }
-    
-    
-    
-  /*  public static class Loginn {
-    	static int a;
-		static int b;
-		static boolean c;
-		static boolean d;
-        public static boolean authenticate(String username, String password) {
-        
-        	//Verlgeiche ob das Passwort bei gegebenen Benutzernamen stimmt und ob sie in der gleichen Stelle gespeichert wurden.
-        	for(String u : LoginDialog.User){
-        		if(u.equals(username)){
-        			  c = true;
-        			  
-        			   a = User.indexOf(username);
-        			
-        		}
-        	}
-        	for(String p : LoginDialog.Passwort){
-            	if(p.equals(password)){
-            		d = true; 
-            		
-            		  
-            		b = User.indexOf(password);
-            		
-            	}
-        	}
-            	if(c==true && d==true && c==d){
-            		return true;}
-            	else
-            		return false;    			
-	
+	private JPasswordField pfPassword;
+	public JLabel lbUsername;
+	private JLabel lbPassword;
+	private JButton btnLogin;
+	private JButton btnAnmelden;
+	public static boolean succeeded;
+	public static boolean loginerfolgreich;
+	public static boolean zulaessigeDaten;
+	public String passwortname;
 
-        }
-
-    }*/
-     
+	/**
+	 * <Die Methode LoginDialog erstellt ein neues JPanel. Dem JPanel werden JButtons und JTextField hinzugefeugt.>
+	 * @param parent das Frame auf dem der Dialog ausgefuehrt wird
+	 * @param fenster fenster von HindiBones 
+	 * @author Simon Nietz, Matr_Nr: 5823560
+	 */
 	
- 
+	public LoginDialog(Frame parent, HindiBones fenster) {
+		super(parent, "Login", true);
+		this.fenster = fenster;
+
+		// erzeuge ein neues JPanel
+		// ordne die Buttons mithilfe GridBagLayout
+		JPanel panel = new JPanel(new GridBagLayout());
+		GridBagConstraints cs = new GridBagConstraints();
+
+		cs.fill = GridBagConstraints.HORIZONTAL;
+
+		// erstelle die Buttons Benutername, Passwort mit den zugehörigen
+		// Textfeldern
+		lbUsername = new JLabel("Benutzername: ");
+		cs.gridx = 0;
+		cs.gridy = 0;
+		cs.gridwidth = 1;
+		panel.add(lbUsername, cs);
+
+		tfUsername = new JTextField(20);
+		cs.gridx = 1;
+		cs.gridy = 0;
+		cs.gridwidth = 2;
+		panel.add(tfUsername, cs);
+
+		lbPassword = new JLabel("Passwort: ");
+		cs.gridx = 0;
+		cs.gridy = 1;
+		cs.gridwidth = 1;
+		panel.add(lbPassword, cs);
+
+		pfPassword = new JPasswordField(20);
+		cs.gridx = 1;
+		cs.gridy = 1;
+		cs.gridwidth = 2;
+		panel.add(pfPassword, cs);
+		panel.setBorder(new LineBorder(Color.GRAY));
+
+		// Fuege den JButton Login und den ActionListener  hinzu
+		btnLogin = new JButton("Login");
+		
+		btnLogin.addActionListener(new ActionListener() {
+
+			/**
+			 * <ActionListener fuer den JButton Login>
+			 * @author Simon Nietz, Matr_Nr: 5823560
+			 */
+			public void actionPerformed(ActionEvent e) {
+				if (getUsername().equals("") || getPassword().equals("")) {
+					JOptionPane.showMessageDialog(LoginDialog.this,
+							"Gib einen Benutzernamen und ein Passwort ein!",
+							"Anmelden", JOptionPane.ERROR_MESSAGE);
+				} else {
+					try {
+						// Ueberpruefe, ob die Daten zulaessig sind
+						zulaessigeDaten = fenster.engine.login(2, getUsername(),
+								getPassword());
+					} catch (InterruptedException e1) {
+					}
+					if (zulaessigeDaten == true) {
+						// Falls die Daten zulaessig sind, wird der Benutzer eingeloggt
+						JOptionPane.showMessageDialog(LoginDialog.this, "Hi "
+								+ getUsername()
+								+ "! Du hast dich erfolgreich eingeloggt.",
+								"Login", JOptionPane.INFORMATION_MESSAGE);
+						loginerfolgreich = true;
+
+						dispose();
+					} else {
+						// Falls nicht, wird eine Error Messag angezeigt
+						JOptionPane.showMessageDialog(LoginDialog.this,
+								"Ungültiger Benutzername oder Passwort",
+								"Login", JOptionPane.ERROR_MESSAGE);
+						// Benutzername und Passwort zuruecksetzen
+						tfUsername.setText("");
+						pfPassword.setText("");
+						loginerfolgreich = false;
+					}
+				}
+			}
+		});
+		
+
+		// neuen Benutzer erstellen
+		btnAnmelden = new JButton("Neu Anmelden");
+		btnAnmelden.addActionListener(new ActionListener() {
+			/**
+			 * <Fuege den ActionListener fuer den JButtoen Neu Anmelden hinzu>
+			 * @author Simon Nietz, Matr_Nr: 5823560
+			 */
+			public void actionPerformed(ActionEvent e) {
+				if (getUsername().equals("") || getPassword().equals("")) {
+					JOptionPane.showMessageDialog(LoginDialog.this,
+							"Gib einen Benutzernamen und ein Passwort ein!",
+							"Anmelden", JOptionPane.ERROR_MESSAGE);
+				} else {
+
+					try {
+						// Ueberpruefe erneut ob die Daten zulaessig sind
+						zulaessigeDaten = fenster.engine.login(1, getUsername(),
+								getPassword());
+					} catch (InterruptedException e1) {
+					}
+
+					if (zulaessigeDaten == true) {
+						// Falls die Daten zulaessig sind, wird ein neuer Benutzer angelegt
+						JOptionPane.showMessageDialog(LoginDialog.this, "Hi "
+								+ getUsername()
+								+ "! Du hast dich erfolgreich neu angemeldet",
+								"Anmeldung", JOptionPane.INFORMATION_MESSAGE);
+						loginerfolgreich = false;
+						tfUsername.setText("");
+						pfPassword.setText("");
+					} else {
+						// Falls die Daten nicht zulaessig sind, wird eine Error Message angezeigt
+						JOptionPane.showMessageDialog(LoginDialog.this,
+								"Der Benutzername ist bereits vergeben!",
+								"Login", JOptionPane.ERROR_MESSAGE);
+						loginerfolgreich = false;
+					}
+				}
+			}
+
+		});
+		
+		// Fuege die Buttons und das Panel hinzu
+		JPanel bp = new JPanel();
+		bp.add(btnLogin);
+		bp.add(btnAnmelden);
+		
+		// Setze die Position des Buttons und das Panel hinzu
+		getContentPane().add(panel, BorderLayout.CENTER);
+		getContentPane().add(bp, BorderLayout.PAGE_END);
+
+		pack();
+		setResizable(false);
+		setLocationRelativeTo(parent);
+	}
+
+	/**
+	 * <Gibt den Text aus dem JTextField zurueck>
+	 * @return gibt den Text im JTextField zurueck
+	 * @author Simon Nietz, Matr_Nr: 5823560
+	 */
+	public static String getUsername() {
+		return tfUsername.getText().trim();
+	}
+
+	/**
+	 * <Gibt den String aus dem JPasswordField zurueck>
+	 * @return das Passwort als String
+	 * @author Simon Nietz, Matr_Nr: 5823560
+	 */
+	public String getPassword() {
+		return new String(pfPassword.getPassword());
+
+	}
+
+	public static boolean isSucceded() {
+		return loginerfolgreich;
+	}
+
 }

a5e0e4a034355715720ffad728121f957177c4c9
Markus <Markus@192.168.2.131>
2016-07-13 16:00:44 +0200

    Kommentare ergaenzt

--- Sweet Sixteen/src/pp2016/team16/server/map/Felder.java
+++ Sweet Sixteen/src/pp2016/team16/server/map/Felder.java
@@ -1,6 +1,18 @@
 package pp2016.team16.server.map;
 
+
+/**
+* <Die Klasse "Felder" beinhaltet die statischen Attribute, auf die die Klasse "AlleFelder" zugreift, um ein Labyrinth
+* bzw. Level zu erzeugen.>
+* @author <Noll, Markus, 5812500 > */
+
 public class Felder {
+	
+	/**
+	 * 8 Konstanten
+	 * 
+	 * @author < Noll, Markus, 5812500 >
+	 */
 	
 	public static final int WAND = 0;
 	public static final int BODEN = 1;

6c158a42df0eba8c1d4b85be6fd75a9d3a4e3172
Enes03 <enes03200603@hotmail.de>
2016-07-13 20:36:56 +0200

    Add files via upload

--- Sweet Sixteen/src/pp2016/team16/shared/Monster.java
+++ Sweet Sixteen/src/pp2016/team16/shared/Monster.java
@@ -1,22 +1,18 @@
 package pp2016.team16.shared;
 
-import pp2016.team16.client.engine.ClientEngine;
-import pp2016.team16.client.gui.HindiBones;
 import pp2016.team16.server.engine.Astern;
-import pp2016.team16.server.engine.ServerEngine;
-import pp2016.team16.server.engine.Wegpunkt;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.LinkedList;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
 
-//import astern.Astern;
-//import astern.Wegpunkt;
-/* Team16: Sweet sixteen
- * Goekdag, Enes, 5615399
+
+/** Team16: Sweet sixteen
+ * Monster-Klasse fuer die Monster in HindiBones.
+ * 
+ * @author Goekdag, Enes, 5615399
  * 
  * */
 public class Monster extends Figur {
@@ -27,20 +23,23 @@ public class Monster extends Figur {
 	public int cooldownWalk;
 
 	public int dir; // Laufrichtung: 0 Nord, 1 Ost, 2 Sued, 3 West
-	private int typ; // Von Beginn an anwesend: 0, Erscheint sp�ter: 1
+	private int typ; // Von Beginn an anwesend: 0, Erscheint spaeter: 1, Hat den Schluessel: 2
 	public Astern astern;
 	public boolean nachricht = true;
 	public boolean angriff =false;
 	
 	
-	/* Team16: Sweet sixteen
-	 * Goekdag, Enes, 5615399
-	 * 
-	 * */
-	private int zustand;// 1:ruhe,2:Spieler jagen;3:fl�chten                       // STATES
+	
+	private int zustand;// 1:ruhe,2:Spieler jagen;3:fluechten                       // STATES
 
 	
-
+	/** Team16: Sweet sixteen
+	 * Konstruktor der Monster Klasse. Es werden die x- und y-Koordinaten der Startposition des Monsters
+	 * uebergeben sowie der Typ das Monsters.
+	 * 
+	 * @author Goekdag, Enes, 5615399
+	 * 
+	 * */
 	public Monster(int x, int y, int typ) {
 		this.typ = typ;
 		setPos(x, y);
@@ -68,19 +67,21 @@ public class Monster extends Figur {
 	}
 	
 	
-	
-	
-	
-
-	
-
-	
-
+	/**
+	 * 
+	 * @return Bruchzahl, welche wiedergibt wie viel Prozent des Cooldowns vorbei ist.
+	 * @author Diese Methoden wurde aus dem alten Spiel uerbernommen
+	 */
 	public double cooldownProzent() {
 		return 1.0 * (System.currentTimeMillis() - lastAttack) / cooldownAttack;
 	}
 
-	// Bewege das Monster
+	/**
+	 * Funktion welche das Monster entsprechend der vorgegebenen Richtung bewegt, sofern die Bewegung
+	 * zulaessig ist und der Bewegungs-Cooldown vorbei ist.
+	 * 
+	 * @author Diese Methoden wurde aus dem alten Spiel uerbernommen
+	 */
 	public void move(boolean zulaessig) {
 	
 		boolean nextWalk = (System.currentTimeMillis() - lastStep) >= cooldownWalk;
@@ -111,7 +112,11 @@ public class Monster extends Figur {
 		}
 	}
 
-	// Laufrichtung des Monsters aendern                                          // HERUMIRREN (NORMAL-ZUSTAND)
+	/**
+	 * Setzt eine zufaellige neue Richtung fuer das Monster.
+	 * 
+	 * @author Diese Methoden wurde aus dem alten Spiel uerbernommen
+	 */
 	public void changeDir() {
 		Random random = new Random();
 		dir = random.nextInt(4);
@@ -121,18 +126,12 @@ public class Monster extends Figur {
 		return typ;
 	}
 
-
-	public LinkedList<int[]> findeWeg(int[] startpunkt, int[] endpunkt) {
-		LinkedList<int[]>besterWeg = new LinkedList<int[]>();
-
-		return besterWeg;
-
-	}
-	/* Team16: Sweet sixteen
-	 * Goekdag, Enes, 5615399
+	/** Team16: Sweet sixteen
+	 * Bestimmt den aktuellen Zustand des Monsters und gibt ihn zurueck.
 	 * 
+	 * @author Goekdag, Enes, 5615399
+	 * @return Der aktuelle Zustand des Monsters
 	 * */
-	
 	public int aktuellenZustandBestimmen(int spielerX, int spielerY) {
 		// Ist der Spieler im Radius des Monsters?                                             // endlicher AUTOMAT- 3 STATES
 		boolean spielerImRadius = (Math.sqrt(Math.pow(spielerX - this.getXPos(), 2)

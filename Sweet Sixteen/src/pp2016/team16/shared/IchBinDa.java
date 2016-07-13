ab83e4d470cee8dae64d14eadeac58ed3eb6f759
Ann-Catherine <Ann-Catherine@AnCa-Laptop>
2016-07-13 20:08:54 +0200

    IchbinDa Nachricht

--- Sweet Sixteen/src/pp2016/team16/shared/IchBinDa.java
+++ Sweet Sixteen/src/pp2016/team16/shared/IchBinDa.java
@@ -7,7 +7,7 @@ package pp2016.team16.shared;
 public class IchBinDa extends MessageObject{
 
 	/**
-	 *generierete serialVerionID
+	 *generierte serialVerionID
 	 */
 	private static final long serialVersionUID = -4801926308794934580L;
 	/**
